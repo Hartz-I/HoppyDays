5e31a1f8925fe264d9013a08e2e560fbde702963
Hartz-I <ahnafsadikinan@gmail.com>
2021-03-03 14:39:52 +0600

    added animation to the bunny

--- notes/before animation/Player/Player.gd
+++ notes/before animation/Player/Player.gd
@@ -1,10 +1,10 @@
 extends KinematicBody2D
 
 var vel = Vector2(0,0) #a vector to run on move and slide
-const SPEED = 1500 #a constant speed
+const SPEED = 1000 #a constant speed
 const GRAVITY = 300
 const UP = Vector2(0,-1) #used to tell this is up or down
-const JUMP_SPEED = 4000
+const JUMP_SPEED = 3000
 
 func _physics_process(delta): #for processing every frame. delta is time
 	#vel.y += GRAVITY #BUT putting it increases every frame! not while falling
@@ -14,8 +14,6 @@ func _physics_process(delta): #for processing every frame. delta is time
 	jump()
 	
 	move()
-	
-	animate()
 	
 	
 	move_and_slide(vel , UP) #move and slide on surface by how much value given in the vector
@@ -27,7 +25,7 @@ func apply_gravity():
 		vel.y += GRAVITY
 		
 func jump():
-	if Input.is_action_pressed("jump") and is_on_floor(): #jump when pressed. and holding it won't work!
+	if Input.is_action_just_pressed("jump") and is_on_floor(): #jump when pressed. and holding it won't work!
 		vel.y -= JUMP_SPEED
 		
 func move():
@@ -41,17 +39,5 @@ func move():
 
 	if Input.is_action_pressed("left") and Input.is_action_pressed("right"):
 		vel.x=0
-
-func animate():
-	#animatedSprite - > frame -> spriteframe -> click again to get animator!
-	if vel.y<0:
-		$AnimatedSprite.play("jump")	 #play the jump animation
-	elif vel.x>0:
-		$AnimatedSprite.play("walk_right")
-		$AnimatedSprite.flip_h = false
-	elif vel.x<0:
-		$AnimatedSprite.play("walk_left")
-		$AnimatedSprite.flip_h = true
-	else: 
-		$AnimatedSprite.play("idle")
+	
 	
