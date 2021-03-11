dd78674c548fce094d207bd313abcd03b68a1ceb
Hartz-I <ahnafsadikinan@gmail.com>
2021-03-12 05:41:30 +0600

    added start menu credits and exit button

--- Levels/Intro.gd
+++ Levels/Intro.gd
@@ -3,3 +3,11 @@ extends Control
 
 func _on_TextureButton_pressed():
 	get_tree().change_scene("res://Levels/Level1.tscn")
+
+
+func _on_TextureButton3_pressed():
+	get_tree().quit()
+
+
+func _on_TextureButton2_pressed():
+	get_tree().change_scene("res://Levels/About.tscn")
